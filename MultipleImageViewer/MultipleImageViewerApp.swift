2ab0ff58818acc02551eb89f174fa72692014b4e
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-10 21:23:40 +0300

    Initial Commit

---
+++ MultipleImageViewer/MultipleImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleImageViewerApp.swift
+//  MultipleImageViewer
+//
+//  Created by Abdullah Karaboğa on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultipleImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
